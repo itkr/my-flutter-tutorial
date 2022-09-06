71d010f164da048644814ecfec297e90267183b2
Shota Itakura <1531860+itkr@users.noreply.github.com>
2022-09-06 11:17:31 +0900

    auto format

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -47,7 +46,7 @@ class MyApp extends StatelessWidget {
                 ],
               ),
             ],
-          )
+          ),
         ),
       ),
     );
@@ -67,10 +66,9 @@ class RandomWordsState extends State<RandomWords> {
   Widget _buildRow(WordPair pair) {
     return ListTile(
         title: Text(
-          pair.asPascalCase,
-          style: _biggerFont,
-        )
-    );
+      pair.asPascalCase,
+      style: _biggerFont,
+    ));
   }
 
   Widget _buildSuggestions() {
@@ -78,7 +76,7 @@ class RandomWordsState extends State<RandomWords> {
       padding: const EdgeInsets.all(16.0),
       itemBuilder: (context, i) {
         if (i.isOdd) return Divider();
-        final index = i~/2;
+        final index = i ~/ 2;
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
