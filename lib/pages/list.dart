e30ab07b1845d324482bc97f8471b246e4c9a2a2
Shota Itakura <1531860+itkr@users.noreply.github.com>
2022-09-12 16:20:10 +0900

    Providerを使う

--- lib/pages/list.dart
+++ lib/pages/list.dart
@@ -9,65 +9,64 @@ class ListWords extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        title: const Text('Welcome to Flutter.'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.favorite, color: Colors.white),
-            onPressed: () {
-              _scaffoldKey.currentState?.openEndDrawer();
-            },
-          ),
-        ],
-        leading: const BackButton(),
-      ),
-      endDrawer: const Drawer(
-        child: FavoriteWidget(),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(1.0),
-        child: const RandomWords(),
+    return ChangeNotifierProvider(
+      create: (context) => RandomWordsStore(),
+      child: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter.'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.favorite, color: Colors.white),
+              onPressed: () {
+                _scaffoldKey.currentState?.openEndDrawer();
+              },
+            ),
+          ],
+          leading: const BackButton(),
+        ),
+        endDrawer: const Drawer(
+          child: FavoriteWidget(),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(1.0),
+          child: const RandomWords(),
+        ),
       ),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class RandomWordsStore with ChangeNotifier {
+  final suggestions = <WordPair>[];
+  final favoriteIndexes = <int>[];
+
+  void addAllSuggestions(Iterable<WordPair> wordPairs) {
+    suggestions.addAll(wordPairs);
+    notifyListeners();
+  }
+
+  void setFavorite(index) {
+    if (favoriteIndexes.contains(index)) {
+      favoriteIndexes.remove(index);
+    } else {
+      favoriteIndexes.add(index);
+    }
+    notifyListeners();
+  }
+}
+
+class RandomWords extends StatelessWidget {
   const RandomWords({Key? key}) : super(key: key);
 
   @override
-  RandomWordsState createState() => RandomWordsState();
-}
+  Widget build(BuildContext context) {
+    final addAllSuggestions =
+        context.read<RandomWordsStore>().addAllSuggestions;
+    final favoriteIndexes = context.read<RandomWordsStore>().favoriteIndexes;
+    final suggestions = context.read<RandomWordsStore>().suggestions;
+    final setFavorite = context.watch<RandomWordsStore>().setFavorite;
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _favoriteIndexes = <int>[];
-
-  Widget _buildRow(WordPair pair, int index) {
-    IconData iconData = _favoriteIndexes.contains(index)
-        ? Icons.favorite
-        : Icons.favorite_outline;
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: const TextStyle(fontSize: 18.0),
-      ),
-      trailing: Icon(iconData),
-      onTap: () {
-        setState(() {
-          if (_favoriteIndexes.contains(index)) {
-            _favoriteIndexes.remove(index);
-          } else {
-            _favoriteIndexes.add(index);
-          }
-        });
-      },
-    );
-  }
-
-  Widget _buildSuggestions() {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemBuilder: (context, i) {
@@ -75,45 +74,47 @@ class RandomWordsState extends State<RandomWords> {
           return const Divider();
         }
         final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
+        if (index >= suggestions.length) {
+          addAllSuggestions(generateWordPairs().take(10));
         }
-        return _buildRow(_suggestions[index], index);
+        IconData iconData = favoriteIndexes.contains(index)
+            ? Icons.favorite
+            : Icons.favorite_outline;
+        return ListTile(
+          title: Text(
+            suggestions[index].asPascalCase,
+            style: const TextStyle(fontSize: 18.0),
+          ),
+          trailing: Icon(iconData),
+          onTap: () {
+            setFavorite(index);
+          },
+        );
       },
     );
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return _buildSuggestions();
-  }
 }
 
-class FavoriteWidget extends StatefulWidget {
+class FavoriteWidget extends StatelessWidget {
   const FavoriteWidget({Key? key}) : super(key: key);
 
   @override
-  FavoriteState createState() {
-    return FavoriteState();
-  }
-}
-
-class FavoriteState extends State {
-  @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <ListTile>[
-        ListTile(
-          title: const Text('a', style: TextStyle(fontSize: 18.0)),
-          trailing: const Icon(Icons.favorite),
-          onTap: () {},
+    final suggestions = context.read<RandomWordsStore>().suggestions;
+    final favoriteIndexes = context.read<RandomWordsStore>().favoriteIndexes;
+
+    favoriteIndexes.sort();
+    List<Widget> children = [];
+    for (int favoriteIndex in favoriteIndexes) {
+      children.add(ListTile(
+        title: Text(
+          suggestions[favoriteIndex].asPascalCase,
+          style: const TextStyle(fontSize: 18.0),
         ),
-        ListTile(
-          title: const Text('b', style: TextStyle(fontSize: 18.0)),
-          trailing: const Icon(Icons.favorite),
-          onTap: () {},
-        ),
-      ],
-    );
+        trailing: const Icon(Icons.favorite),
+        onTap: () {},
+      ));
+    }
+    return ListView(children: children);
   }
 }
